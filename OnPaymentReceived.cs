c321faf42bc9f8d59feab9cf94f4612f9b93760a
Mark Heath <mark.heath@gmail.com>
2019-01-26 22:07:22 +0000

    Adding a queue output binding

--- OnPaymentReceived.cs
+++ OnPaymentReceived.cs
@@ -15,12 +15,14 @@ namespace pluralsightfuncs
         [FunctionName("OnPaymentReceived")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
             ILogger log)
         {
             log.LogInformation("Received a payment.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             var order = JsonConvert.DeserializeObject<Order>(requestBody);
+            await orderQueue.AddAsync(order);
             log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
             return new OkObjectResult($"Thank you for your purchase");
         }
