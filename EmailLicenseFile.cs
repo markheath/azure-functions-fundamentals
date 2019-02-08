959f2b738d0f63045494cb56a5d20cc66cffb92c
Mark Heath <mark.heath@gmail.com>
2019-02-08 20:55:57 +0000

    using IBinder demo

--- EmailLicenseFile.cs
+++ EmailLicenseFile.cs
@@ -11,23 +11,25 @@ namespace pluralsightfuncs
     public static class EmailLicenseFile
     {
         [FunctionName("EmailLicenseFile")]
-        public static void Run([BlobTrigger("licenses/{name}", 
+        public static void Run([BlobTrigger("licenses/{orderId}.lic", 
             Connection = "AzureWebJobsStorage")]string licenseFileContents,
-            [SendGrid(ApiKey = "SendGridApiKey")] out SendGridMessage message,
-            string name, 
+            [SendGrid(ApiKey = "SendGridApiKey")] ICollector<SendGridMessage> sender,
+            [Table("orders", "orders", "{orderId}")] Order order,
+            string orderId, 
             ILogger log)
         {
-            var email = Regex.Match(licenseFileContents,
-                @"^Email\:\ (.+)$", RegexOptions.Multiline).Groups[1].Value;
-            log.LogInformation($"Got order from {email}\n License file Name:{name}");
-            message = new SendGridMessage();
+            var email = order.Email;
+            log.LogInformation($"Got order from {email}\n Order Id:{orderId}");
+            var message = new SendGridMessage();
             message.From = new EmailAddress(Environment.GetEnvironmentVariable("EmailSender"));
             message.AddTo(email);
             var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(licenseFileContents);
             var base64 = Convert.ToBase64String(plainTextBytes);
-            message.AddAttachment(name, base64, "text/plain");
+            message.AddAttachment($"{orderId}.lic", base64, "text/plain");
             message.Subject = "Your license file";
             message.HtmlContent = "Thank you for your order";
+            if (!email.EndsWith("@test.com"))
+                sender.Add(message);
         }
     }
 }
--- GenerateLicenseFile.cs
+++ GenerateLicenseFile.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -9,11 +10,17 @@ namespace pluralsightfuncs
     public static class GenerateLicenseFile
     {
         [FunctionName("GenerateLicenseFile")]
-        public static void Run(
+        public static async Task Run(
             [QueueTrigger("orders", Connection = "AzureWebJobsStorage")]Order order,
-            [Blob("licenses/{rand-guid}.lic")] TextWriter outputBlob,
+            IBinder binder,
             ILogger log)
         {
+            var outputBlob = await binder.BindAsync<TextWriter>(
+                new BlobAttribute($"licenses/{order.OrderId}.lic")
+                {
+                    Connection = "AzureWebJobsStorage"
+                });
+
             outputBlob.WriteLine($"OrderId: {order.OrderId}");
             outputBlob.WriteLine($"Email: {order.Email}");
             outputBlob.WriteLine($"ProductId: {order.ProductId}");
