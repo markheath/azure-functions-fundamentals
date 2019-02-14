63da9c1037ac91eda29495ceb301eebcb8c87fb1
Mark Heath <mark.heath@gmail.com>
2019-02-14 20:32:36 +0000

    deployment demo script - ARM template

---
+++ arm-deploy-function-app.ps1
@@ -0,0 +1,25 @@
+# create a resource group
+$resourceGroup = "pluralsightfuncsarm"
+$location = "westeurope"
+az group create -n $resourceGroup -l $location
+
+# choose a name for our function app
+$functionAppName = "pluralsightfuncsarm"
+
+# to check our template syntax
+az group deployment validate -g $resourceGroup --template-file azuredeploy.json `
+    --parameters appName=$functionAppName
+
+# to actually deploy our function app
+az group deployment create -g $resourceGroup --template-file azuredeploy.json `
+    --parameters appName=$functionAppName
+
+# see what's in the resource group we just created
+az resource list -g $resourceGroup -o table
+
+# check the app settings were configured correctly
+az functionapp config appsettings list `
+    -n $functionAppName -g $resourceGroup -o table
+
+# to clean up when we're done
+az group delete -n $resourceGroup --no-wait -y
